328890ca0837b6a14c6c8823477160158efdb95d
mauro-fanton <mauro.fanton@opencastsoftware.com>
2023-08-07 20:54:05 +0100

    addes some comment

--- Smis.Registration.Persistence.Lib/Address.cs
+++ Smis.Registration.Persistence.Lib/Address.cs
@@ -7,24 +7,39 @@ namespace Smis.Registration.Persistence.Lib
     [BsonDiscriminator("address")]
     public class Address
 	{
+        /// <summar>
+        /// 1st line of address
+        /// </summary>
         [BsonElement("addressLine1")]
         [BsonRequired]
         [Required]
         public string AddressLine1 { get; set; }
 
+        /// <summary>
+        /// 2nd line of address
+        /// </summary>
         [BsonElement("addressLine2")]
         [BsonRequired]
         [Required]
         public string AddressLine2 { get; set; }
 
+        /// <summary>
+        /// 3rd line of address
+        /// </summary>
         [BsonElement("addressLine3")]
         [BsonIgnoreIfNull]
         public string? AddressLine3 { get; set; }
 
+        /// <summary>
+        /// 4th line of address
+        /// </summary>
         [BsonElement("addressLine4")]
         [BsonIgnoreIfNull]
         public string? AddressLine4 { get; set; }
 
+        /// <summary>
+        /// Post code
+        /// </summary>
         [BsonElement("postCode")]
         [BsonRequired]
         [Required]
--- Smis.Registration.Persistence.Lib/Application.cs
+++ Smis.Registration.Persistence.Lib/Application.cs
@@ -11,62 +11,101 @@ namespace Smis.Registration.Persistence.Lib
         [BsonIgnore]
         public const string TableName = "application";
 
+        /// <summary>
+        /// The unique record id
+        /// </summary>
         public Guid Id { get; set; }
 
+        /// <summary>
+        /// First name
+        /// </summary>
         [BsonElement("firstName")]
         [BsonRequired]
         [Required]
         public string FirstName { get; set; }
 
+        /// <summary>
+        /// Surname
+        /// </summary>
         [BsonElement("surname")]
         [BsonRequired]
         [Required]
         public string Surname { get; set; }
 
+        /// <summary>
+        /// Date of birth
+        /// </summary>
         [BsonElement("dob")]
         [BsonDateTimeOptions]
         [BsonRequired]
         [Required]
         public DateTime DateOfBirthday { get; set ; }
 
+        /// <summary>
+        /// Unique application number
+        /// </summary>
         [BsonElement("applicationNumber")]
         [BsonRequired]
         [Required]
         public string ApplicationNumber { get; set; }
 
+        /// <summary>
+        /// Title
+        /// </summary>
         [BsonElement("title")]
         [BsonRequired]
         [Required]
         public string Title { get; set; }
 
+        /// <summary>
+        /// Contact address
+        /// </summary>
         [BsonElement("contactAddress")]
         [BsonRequired]
         [Required]
         public Address ContactAddress { get; set; }
 
+        /// <summary>
+        /// Primary guardian name
+        /// </summary>
         [BsonElement("primaryGuardianName")]
         [BsonRequired]
         [Required]
         public string PrimaryGuardianName { get; set; }
 
+        /// <summary>
+        /// Primary guardian surname
+        /// </summary>
         [BsonElement("primaryGuardianSurname")]
         [BsonRequired]
         [Required]
         public string PrimaryGuardianSurname { get; set; }
 
+        /// <summary>
+        /// Primary guardian telephone number
+        /// </summary>
         [BsonElement("primaryGuardianTelephone")]
         [BsonRequired]
         [Required]
         public string PrimaryGuardianTelephone { get; set; }
 
+        /// <summary>
+        /// Secondaru guardian name
+        /// </summary>
         [BsonElement("secondaryGuardianName")]
         [BsonIgnoreIfNull]
         public string? SecondaryGuardianName { get; set; }
 
+        /// <summary>
+        /// Secondary guardian surname
+        /// </summary>
         [BsonElement("secondaryGuardianSurname")]
         [BsonIgnoreIfNull]
         public string? SecondaryGuardianSurname { get; set; }
 
+        /// <summary>
+        /// Secondary guardian telephone number
+        /// </summary>
         [BsonElement("secondaryGuardianTelephone")]
         [BsonIgnoreIfNull]
         public string? SecondaryGuardianTelephone { get; set; }       
